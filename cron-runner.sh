9d6591ba390fbab699d5a5f1ea70074cba4e3aee
Frear <software_email_addr_is_potentially_spammed_via_github@frear.com>
2021-07-14 12:27:53 -0700

    cron-runner.sh: Add descriptive text

--- cron-runner.sh
+++ cron-runner.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# This is a quick and dirty way to effective daemonize the
+# notify-if-unlocked.py program.
+#
+# It has been tested with the following cron entry:
+#
+# Start the august door lock scanner program as a daemon
+# 0 * * * * dev/2021-07-10-august-door-lock/j-src/cron-runner.sh >> dev/2021-07-10-august-door-lock/j-src/cron-output 2>&1
+
 if [ $(ps xo args | egrep -c '^python -u \./notify-if-unlocked\.py') -ne 0 ]; then exit 0 ; fi
 
 source ~/dev/2021-07-10-august-door-lock/august/bin/activate
