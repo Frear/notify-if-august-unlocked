157d3ce4242a7ffc83dc5e1bf5329433e34e3b3c
Frear <software_email_addr_is_potentially_spammed_via_github@frear.com>
2021-07-15 15:15:37 -0700

    cron-runner.sh: Change a directory name

--- cron-runner.sh
+++ cron-runner.sh
@@ -6,10 +6,10 @@
 # It has been tested with the following cron entry:
 #
 # Start the august door lock scanner program as a daemon
-# 0 * * * * dev/2021-07-10-august-door-lock/j-src/cron-runner.sh >> dev/2021-07-10-august-door-lock/j-src/cron-output 2>&1
+# 0 * * * * dev/2021-07-10-august-door-lock/notify-if-unlocked/cron-runner.sh >> dev/2021-07-10-august-door-lock/notify-if-unlocked/cron-output 2>&1
 
 if [ $(ps xo args | egrep -c '^python -u \./notify-if-unlocked\.py') -ne 0 ]; then exit 0 ; fi
 
 source ~/dev/2021-07-10-august-door-lock/august/bin/activate
-cd ~/dev/2021-07-10-august-door-lock/j-src || exit 1
+cd ~/dev/2021-07-10-august-door-lock/notify-if-unlocked || exit 1
 python -u ./notify-if-unlocked.py -c ./send-sns-notification.sh "$@"
