3f15b8476101f1643da780fedb066885edad5c81
Frear <software_email_addr_is_potentially_spammed_via_github@frear.com>
2023-09-11 22:19:39 -0700

    Update cron-runner.sh to do the same thing but have fewer backgrounded pids

--- cron-runner.sh
+++ cron-runner.sh
@@ -10,6 +10,6 @@
 
 if [ $(ps xo args | egrep -c '^python -u \./notify-if-unlocked\.py') -ne 0 ]; then exit 0 ; fi
 
-source ~/dev/2021-07-10-august-door-lock/august/bin/activate
+source ~/dev/2021-07-10-august-door-lock/august/bin/activate && \
 cd ~/dev/2021-07-10-august-door-lock/notify-if-unlocked || exit 1
-python -u ./notify-if-unlocked.py -c ./send-sns-notification.sh "$@"
+python -u ./notify-if-unlocked.py -c ./send-sns-notification.sh "$@" &
